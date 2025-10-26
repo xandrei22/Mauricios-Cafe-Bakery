432006b33cc5dbacd624fc9d5cdcd94614d864be
unknown <alexandrei1628@gmail.com>
2025-10-27 02:25:25 +0800

    pssport

--- backend/controllers/passport.js
+++ backend/controllers/passport.js
@@ -32,7 +32,7 @@ module.exports = (passport, db) => {
         passport.use(new GoogleStrategy({
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5001/api/auth/google/callback'
+                callbackURL: process.env.GOOGLE_CALLBACK_URL
             },
             async(accessToken, refreshToken, profile, done) => {
                 try {
