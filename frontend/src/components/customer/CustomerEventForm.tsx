c5800769a497158b3e388304de38019b094ad3a4
unknown <alexandrei1628@gmail.com>
2025-11-11 13:18:50 +0800

    fix

--- frontend/src/components/customer/CustomerEventForm.tsx
+++ frontend/src/components/customer/CustomerEventForm.tsx
@@ -356,7 +356,7 @@ const CustomerEventForm: React.FC<CustomerEventFormProps> = ({ customer_id, cust
               value={contactName}
               onChange={e => setContactName(e.target.value)}
               required
-              className="h-12 border border-gray-300 rounded-xl focus:border-gray-400 focus:ring-2 focus:ring-gray-200"
+              className="h-12 !border-gray-300 border rounded-xl focus:!border-gray-400 focus:ring-2 focus:ring-gray-200"
             />
           </div>
           
@@ -369,7 +369,7 @@ const CustomerEventForm: React.FC<CustomerEventFormProps> = ({ customer_id, cust
               value={contactNumber}
               onChange={e => setContactNumber(e.target.value)}
               required
-              className="h-12 border border-gray-300 rounded-xl focus:border-gray-400 focus:ring-2 focus:ring-gray-200"
+              className="h-12 !border-gray-300 border rounded-xl focus:!border-gray-400 focus:ring-2 focus:ring-gray-200"
             />
           </div>
           
